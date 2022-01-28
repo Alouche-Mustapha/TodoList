c7931787bd19d7c4571a671e9b6119732ca15185
BOUCHENTOUF Oussama <oussamabouchentouf6@gmail.com>
2022-01-28 20:01:24 +0100

    KeyboardAvoidingView

--- App.js
+++ App.js
@@ -1,23 +1,33 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { TextInput } from "react-native-paper";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
+import { TextInput } from "react-native";
 
 import { commonColors } from "./src/utils/Colors";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={styles.container}
+    >
       <View style={styles.titleContainer}>
         <Text style={{ fontSize: 30, fontWeight: "bold" }}>Today's tasks</Text>
       </View>
-      <View>
-
+      <View style={styles.bodyContainer}></View>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} />
+        <TouchableOpacity style={styles.button}>
+          <Text>+</Text>
+        </TouchableOpacity>
       </View>
-      <View>
-
-      </View>
-      <StatusBar style="auto" />
-    </View>
+    </KeyboardAvoidingView>
   );
 }
 
@@ -28,10 +38,36 @@ const styles = StyleSheet.create({
     justifyContent: "flex-start",
   },
   titleContainer: {
-    flex: 0,
-    height: "15%",
+    flex: 0.2,
     alignItems: "flex-start",
     justifyContent: "flex-end",
     backgroundColor: commonColors.Green,
+  },
+  bodyContainer: {
+    flex: 1,
+  },
+  inputContainer: {
+    flex: 0.2,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-around",
+    backgroundColor: commonColors.Maroon,
+  },
+  textInput: {
+    flex: 0.9,
+    borderColor: commonColors.Black,
+    borderRadius: 50,
+    height: 50,
+    borderWidth: 3,
+    backgroundColor: "white",
+  },
+  button: {
+    backgroundColor: commonColors.Yellow,
+    borderColor: commonColors.Black,
+    borderRadius: 50,
+    width: 50,
+    height: 50,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
