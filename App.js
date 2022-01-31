95390418af66c545175e293038d50042bbebd198
Alouche-Mustapha <mustapha.alouche@ump.ac.ma>
2022-01-31 22:48:51 +0100

    committing my version of the app

--- App.js
+++ App.js
@@ -1,73 +1,160 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
+  KeyboardAvoidingView,
   StyleSheet,
   Text,
   View,
+  TextInput,
   TouchableOpacity,
-  KeyboardAvoidingView,
-  Platform,
+  Keyboard,
+  ScrollView,
 } from "react-native";
-import { TextInput } from "react-native";
-
+import Task from "./src/components/Task";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { commonColors } from "./src/utils/Colors";
 
 export default function App() {
+  const [task, setTask] = useState();
+  const [taskItems, setTaskItems] = useState([]);
+
+  const storeData = async () => {
+    try {
+      await AsyncStorage.setItem("allTasks", JSON.stringify(taskItems));
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const getData = async () => {
+    try {
+      const tasksHistory = await AsyncStorage.getItem("allTasks");
+      if (tasksHistory && JSON.parse(tasksHistory).length) {
+        setTaskItems(JSON.parse(tasksHistory));
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  useEffect(() => {
+    storeData();
+  }, [taskItems]);
+
+  const handleAddTask = () => {
+    Keyboard.dismiss();
+    setTaskItems([...taskItems, task]);
+    setTask(null);
+  };
+
+  const completeTask = (index) => {
+    let itemsCopy = [...taskItems];
+    itemsCopy.splice(index, 1);
+    setTaskItems(itemsCopy);
+  };
+
   return (
-    <KeyboardAvoidingView
-      behavior={Platform.OS === "ios" ? "padding" : "height"}
-      style={styles.container}
-    >
-      <View style={styles.titleContainer}>
-        <Text style={{ fontSize: 30, fontWeight: "bold" }}>Today's tasks</Text>
-      </View>
-      <View style={styles.bodyContainer}></View>
-      <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} />
-        <TouchableOpacity style={styles.button}>
-          <Text>+</Text>
+    <View style={styles.container}>
+      {/* Added this scroll view to enable scrolling when list gets longer than the page */}
+      <ScrollView
+        contentContainerStyle={{
+          flexGrow: 1,
+        }}
+        keyboardShouldPersistTaps="handled"
+      >
+        {/* Today's Tasks */}
+        <View style={styles.tasksWrapper}>
+          <Text style={styles.sectionTitle}>Today's tasks</Text>
+          <View style={styles.items}>
+            {/* This is where the tasks will go! */}
+            {taskItems.length ? (
+              taskItems.map((item, index) => {
+                return (
+                  <TouchableOpacity
+                    key={index}
+                    onPress={() => completeTask(index)}
+                  >
+                    <Task text={item} />
+                  </TouchableOpacity>
+                );
+              })
+            ) : (
+              <Text>Nothing</Text>
+            )}
+          </View>
+        </View>
+      </ScrollView>
+
+      {/* Write a task */}
+      {/* Uses a keyboard avoiding view which ensures the keyboard does not cover the items on screen */}
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.writeTaskWrapper}
+      >
+        <TextInput
+          style={styles.input}
+          placeholder={"Write a task"}
+          value={task}
+          onChangeText={(text) => setTask(text)}
+        />
+        <TouchableOpacity onPress={() => handleAddTask()}>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
         </TouchableOpacity>
-      </View>
-    </KeyboardAvoidingView>
+      </KeyboardAvoidingView>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: commonColors.Gray,
-    justifyContent: "flex-start",
+    backgroundColor: commonColors.Teal,
   },
-  titleContainer: {
-    flex: 0.2,
-    alignItems: "flex-start",
-    justifyContent: "flex-end",
-    backgroundColor: commonColors.Green,
+  tasksWrapper: {
+    paddingTop: "20%",
+    paddingHorizontal: 20,
+    backgroundColor: "red",
   },
-  bodyContainer: {
-    flex: 1,
+  sectionTitle: {
+    fontSize: 24,
+    fontFamily: "Roboto Bold Italic",
+    fontWeight: "bold",
   },
-  inputContainer: {
-    flex: 0.2,
+  items: {
+    marginTop: 30,
+    backgroundColor: "blue",
+  },
+  writeTaskWrapper: {
+    position: "absolute",
+    bottom: 60,
+    width: "100%",
     flexDirection: "row",
-    alignItems: "center",
     justifyContent: "space-around",
-    backgroundColor: commonColors.Maroon,
+    alignItems: "center",
   },
-  textInput: {
-    flex: 0.9,
-    borderColor: commonColors.Black,
-    borderRadius: 50,
-    height: 50,
-    borderWidth: 3,
-    backgroundColor: "white",
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: "#FFF",
+    borderRadius: 60,
+    borderColor: "#C0C0C0",
+    borderWidth: 1,
+    width: 250,
   },
-  button: {
-    backgroundColor: commonColors.Yellow,
-    borderColor: commonColors.Black,
-    borderRadius: 50,
-    width: 50,
-    height: 50,
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: "#FFF",
+    borderRadius: 60,
     justifyContent: "center",
     alignItems: "center",
+    borderColor: "#C0C0C0",
+    borderWidth: 1,
   },
+  addText: {},
 });
